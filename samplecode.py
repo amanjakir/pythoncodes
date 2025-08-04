9f9a8417fbb7dd506bc0559d0bea18c60b9fb030
amanjakir <amanjakir86@gmail.com>
2025-08-04 17:53:57 +0530

    Update samplecode.py

--- samplecode.py
+++ samplecode.py
@@ -1,2 +1,12 @@
 # heloo
-print("hey")
+# print("hey")
+
+num1 =int(input("Enter the first number:"))
+num2 =int(input("Enter the second number:"))
+if num1 > num2:
+    num1 += 10
+else:
+    num2 -= 5
+    print("Updated first number:",num1)
+    print("Updated second number:",num2)
+
