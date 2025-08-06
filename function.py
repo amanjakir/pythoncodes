74ef42a59fcba5834f7f504a87cbe3d8aefe11b2
amanjakir <amanjakir86@gmail.com>
2025-08-06 12:08:53 +0530

    first comment

---
+++ function.py
@@ -0,0 +1,30 @@
+# def Greetings(name,age):
+#     print("hello",name,"you are",age,"years old")
+# Greetings("jack",32)
+#
+# def is_even(num1):
+#     if num1%2==0:
+#         print(num1,"is even")
+#     else:
+#         print(num1,"is odd")
+# num1=int(input("Enter the number :"))
+# is_even(num1)
+
+# def Addition(x=+,y=12):
+#     print(x+y)
+# a=15
+# b=10
+# Addition(a)
+# Addition(a,b)
+
+# def Factorial(num):
+#     fact =1
+#     for n in range(1, num + 1):
+#         fact = fact * n
+#     print(f"Factorial of {num} is {fact}")
+# num=int(input("Enter the number :"))
+# Factorial(num)
+
+num=int(input("Enter the number :"))
+if num > 1:
+
