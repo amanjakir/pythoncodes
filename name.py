bdcb646c73f616610697f0af16edf76f56bd6406
amanjakir <amanjakir86@gmail.com>
2025-08-26 10:57:16 +0530

    first commit

---
+++ name.py
@@ -0,0 +1,4 @@
+name= input("Enter your name:")
+age=int(input("Enter your age:"))
+favorite_number=int(input("Enter your favorite number:"))
+print(f"Hello, {name}! you are {age} years old,and your favorite number is {favorite_number}.")
